a731c1ca9f4c36cbfbc4faa2dc69c42deda125e6
Chris Syversen <csyversen@gmail.com>
2016-07-15 16:56:14 -0500

    mid chap 7

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -33,14 +33,14 @@ defmodule Rumbl.UserController do
     render conn, "show.html", user: user
   end
 
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: page_path(conn, :index))
-      |> halt()
-    end
-  end
+  # defp authenticate(conn, _opts) do
+  #   if conn.assigns.current_user do
+  #     conn
+  #   else
+  #     conn
+  #     |> put_flash(:error, "You must be logged in to access that page")
+  #     |> redirect(to: page_path(conn, :index))
+  #     |> halt()
+  #   end
+  # end
 end
